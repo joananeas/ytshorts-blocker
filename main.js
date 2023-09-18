9cf694dc0f51974860b4fc5e090f3826f2e5881a
nasneas <joananeasamoraga@gmail.com>
2023-09-18 17:29:22 +0200

    comments

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Joan Aneas (joananeas on Github)
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete" && tab.url === "https://www.youtube.com/shorts") {
       browser.tabs.update(tabId, { url: "https://www.youtube.com" });
