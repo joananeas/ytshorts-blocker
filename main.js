c6fdfb609d886c03eaef34d605d3129cb6753e6a
nasneas <joananeasamoraga@gmail.com>
2023-09-18 17:55:44 +0200

    fix3

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
 // Joan Aneas (joananeas on Github)
-/*browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === "complete" && tab.url.startsWith("https://www.youtube.com/shorts")) {
-      const newUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; //RickRoll for wasting time
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tab.url.startsWith("https://www.youtube.com/shorts")) {
+      const newUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; // Redirige siempre a esta URL
       browser.tabs.update(tabId, { url: newUrl });
     }
+  });
 
-  });
-*/
   //Debugger
-  document.body.style.border = "5px solid red";
-  browser.tabs.update(tabId, { url: newUrl }).then(() => {
-    if (changeInfo.status === "complete" && tab.url.startsWith("https://www.youtube.com/shorts")) {
-      const newUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; //RickRoll for wasting time
-      browser.tabs.update(tabId, { url: newUrl });
-    }
-  }).catch(error => {
-    console.error(error);
-  });
+  //document.body.style.border = "5px solid red";
