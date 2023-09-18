e98e9361429adc5f92305ff872c4b57f7adad77b
nasneas <joananeasamoraga@gmail.com>
2023-09-18 17:48:35 +0200

    fix2

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 // Joan Aneas (joananeas on Github)
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+/*browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete" && tab.url.startsWith("https://www.youtube.com/shorts")) {
-      const newUrl = tab.url.replace(/\/shorts.*$/, ""); 
+      const newUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; //RickRoll for wasting time
       browser.tabs.update(tabId, { url: newUrl });
     }
+
+  });
+*/
+  //Debugger
+  document.body.style.border = "5px solid red";
+  browser.tabs.update(tabId, { url: newUrl }).then(() => {
+    if (changeInfo.status === "complete" && tab.url.startsWith("https://www.youtube.com/shorts")) {
+      const newUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; //RickRoll for wasting time
+      browser.tabs.update(tabId, { url: newUrl });
+    }
+  }).catch(error => {
+    console.error(error);
   });
