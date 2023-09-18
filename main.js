b45fac1f186ef401543d5de71690112648f75acf
nasneas <joananeasamoraga@gmail.com>
2023-09-18 17:33:13 +0200

    fix

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Joan Aneas (joananeas on Github)
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === "complete" && tab.url === "https://www.youtube.com/shorts") {
-      browser.tabs.update(tabId, { url: "https://www.youtube.com" });
+    if (changeInfo.status === "complete" && tab.url.startsWith("https://www.youtube.com/shorts")) {
+      const newUrl = tab.url.replace(/\/shorts.*$/, ""); 
+      browser.tabs.update(tabId, { url: newUrl });
     }
   });
